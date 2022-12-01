d7550a7075b0a4c43177fbb47f626360e3970522
AdarshMitr <adarshmitr@gmail.com>
2022-12-01 18:45:15 +0530

    error handling with 'bind' and 'this'

--- src/components/CfunctionClick.js
+++ src/components/CfunctionClick.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react'
 
 export class CfunctionClick extends Component {
 
-changeEvent(){
-    console.log('Hello Adarsh')
+    constructor(){
+        super();
+
+       // this.changeEvent=this.changeEvent.bind(this);
+//we can simply use arrow function  instead of 'bind' in 'changeEvent function' in order to access 'this'.
+
+        this.state={
+            course:'B.Tech'
+        }
+    }
+
+changeEvent=()=>{
+
+    this.setState({
+        course:'M.Tech'
+    })
+    console.log('Hello Adarsh',this.state.course)
 }
 
   render() {
     return (
       <div>
         <button onClick={this.changeEvent}>Click Me too</button>
+        {this.state.course}
       </div>
     )
   }
