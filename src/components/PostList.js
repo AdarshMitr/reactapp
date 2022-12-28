14a003d87600cf82710606b81825a16df97a7d6a
AdarshMitr <adarshmitr@gmail.com>
2022-12-28 21:17:34 +0530

    HTTP GET Request-MAP Function

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,24 +6,37 @@ constructor(props) {
   super(props)
 
   this.state = {
-    posts:[]
+    posts:[],
+    error:''
   }
 }
 
 componentDidMount(){
-    axios.get("https://jsonplaceholder.typicode.com/postssdsd")
+    axios.get("https://jsonplaceholder.typicode.com/posts")
     .then(response=>{
+      this.setState({posts:response.data});
 console.log(response);
+
     })
     .catch(error=>{
+      this.setState({error:'URL Error'})
 console.log(error);
     })
 }
 
   render() {
+    const {posts,error}=this.state
     return (
       <div>
 List Post Data
+{
+
+  posts.length?
+  posts.map(post=><div key={post.id}>{post.title}</div>) :null
+}
+{
+  error?<div>{error}</div>:null
+}
       </div>
     )
   }
